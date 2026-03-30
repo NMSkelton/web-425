0dddbf9b0640c07bc215bfecfed05f08fb80dd40
Nicholas Skelton <nicholasskelton30@gmail.com>
2026-03-29 23:31:23 -0500

    Week 1 Push

--- rpg-character-builder/src/app/app.component.ts
+++ rpg-character-builder/src/app/app.component.ts
@@ -5,8 +5,53 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  template: `
+    <div class="layout">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Faculty+Glyphic&family=Metamorphous&family=Noto+Serif:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+      </head>
+
+      <header class="banner">
+        <img
+          src="/assets/rpg_banner.png"
+          alt="Banner image for RPG Character Builder"
+          class="banner-img"
+        >
+      </header>
+
+      <nav class="navbar">
+
+        <div class="nav-links">
+          <a href="#">Home</a>
+          <a href="#">Builder</a>
+          <a href="#">Characters</a>
+          <a href="#">About</a>
+        </div>
+      </nav>
+
+      <main class="content">
+        <router-outlet></router-outlet>
+      </main>
+
+      <footer class="footer">
+        <div class="footer-links">
+          <a href="#">Home</a>
+          <a href="#">Builder</a>
+          <a href="#">Characters</a>
+          <a href="#">About</a>
+        </div>
+
+        <p class="copyright">
+          © 2026 RPG Builder
+        </p>
+      </footer>
+
+    </div>
+  `,
+  styles: [`
+  `]
 })
 export class AppComponent {
   title = 'rpg-character-builder';
---
+++ rpg-character-builder/src/app/home/home.component.ts
@@ -0,0 +1,59 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [],
+  template: `
+    <div class="grid-container">
+
+      <div class="grid-item">
+        <img src="/assets/goblin.png" alt="Goblin with a mace" class="grid-image">
+      </div>
+
+      <div class="grid-middle">
+        <h2>Welcome to RPG Character Builder</h2>
+        <p>Looking to plan your character for the next DnD campaign? Or perhaps gain inspiration for an upcoming novel? Well look no further, as you've found RPG Character Builder!</p>
+        <p>Dive into endless possibilities with our immersive character builder! Explore thousands of unique combinations, tweak every detail, or craft a hero entirely from scratch. When your masterpiece is complete, save your character and bring them into your next adventure.</p>
+        <p>Want to build a towering orc blood warrior? We can do that!</p>
+        <p>How about a stealthy elf ranger? A bit generic, but we got you covered!</p>
+        <p>Maybe a pigeon-toed goblin imp that wields a mannequin arm like a club? Firstly, get some help. Secondly, we can do that too!</p>
+        <p>No matter how lofty your imagination is, you can realize it here at RPG Character Builder!
+      </div>
+
+      <div class="grid-item">
+        <img src="/assets/ranger.png" alt="Ranger standing heroically" class="grid-image">
+      </div>
+
+    </div>
+  `,
+  styles: `
+    .grid-container {
+      display: grid;
+      grid-template-columns: 1fr 2fr 1fr;
+      gap: 20px;
+      padding: 20px;
+      min-height: 80vh;
+      align-items: center;
+    }
+
+    .grid-item {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+
+    .grid-image {
+      max-width: 100%;
+      height: auto;
+    }
+
+    .grid-middle {
+      text-align: center;
+      padding: 20%;
+    }
+  `
+})
+export class HomeComponent {
+
+}
